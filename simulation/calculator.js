481acfa7e09d8da0fd62d977bfafd7a0787df23c
yeada <yeadalm04@gmail.com>
2024-05-14 18:30:08 +0900

    Style: 계산하기 버튼 위치 수정

--- simulation/calculator.js
+++ simulation/calculator.js
@@ -503,7 +503,7 @@ function displaySungjuk(data, AtnlcScreSungjukInfo) {
     const calculateButton = document.createElement('button');
     calculateButton.textContent = '계산하기';
     calculateButton.style.padding = '5px 15px';
-    calculateButton.style.marginRight = '110px';
+    calculateButton.style.marginRight = '300px';
     headerContainer.appendChild(calculateButton); // 제목 옆에 계산하기 버튼 추가
 
     containerSimul.appendChild(headerContainer);
